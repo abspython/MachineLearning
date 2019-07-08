071060ce146e746230ac339d88005d73bbd94f77
abspython <abhijithsheheer@gmail.com>
2019-07-08 12:35:48 +0530

    Updated genderclf

---
+++ SoAI(Siraj)/GenderClassifier/README.md
@@ -0,0 +1,36 @@
+### Gender Classifier
+
+##### Dependencies
+
+Need the following libraries
+
+- sklearn
+- numpy
+
+To install it ,
+
+```
+$ pip install sklearn, numpy
+```
+
+
+
+##### Classifiers
+
+Used Classifiers like
+
+1. DecisionTree
+2. KNN
+3. SVC
+4. Perceptron
+5. GuassianNB
+
+and find out the best classifier out of the above mentioned.
+
+
+
+##### Sources/References
+
+For complete details on classifiers, check out in the official documentation on https://scikit-learn.org
+
+Source: https://github.com/Naresh1318/GenderClassifier/blob/master/Run_Code.py
--- SoAI(Siraj)/GenderClassifier/genderclf.py
+++ SoAI(Siraj)/GenderClassifier/genderclf.py
@@ -1,1 +1,53 @@
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import Perceptron
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+import numpy as np
 
+# Data in format [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+# Classifiers with default params
+clf_tree = tree.DecisionTreeClassifier()
+clf_svm = SVC()
+clf_perceptron = Perceptron()
+clf_KNN = KNeighborsClassifier()
+clf_GNB = GaussianNB()
+
+# Training the models
+clf_tree.fit(X, Y)
+clf_svm.fit(X, Y)
+clf_perceptron.fit(X, Y)
+clf_KNN.fit(X, Y)
+clf_GNB.fit(X, Y)
+
+pred_tree = clf_tree.predict(X)
+acc_tree = accuracy_score(Y, pred_tree) * 100
+print('Accuracy for DecisionTree: {}'.format(acc_tree))
+
+pred_svm = clf_svm.predict(X)
+acc_svm = accuracy_score(Y, pred_svm) * 100
+print('Accuracy for SVM: {}'.format(acc_svm))
+
+pred_per = clf_perceptron.predict(X)
+acc_per = accuracy_score(Y, pred_per) * 100
+print('Accuracy for perceptron: {}'.format(acc_per))
+
+pred_KNN = clf_KNN.predict(X)
+acc_KNN = accuracy_score(Y, pred_KNN) * 100
+print('Accuracy for KNN: {}'.format(acc_KNN))
+
+pred_GNB = clf_GNB.predict(X)
+acc_GNB = accuracy_score(Y, pred_GNB) * 100
+print('Accuracy for GaussianNB: {}'.format(acc_GNB))
+
+index = np.argmax([acc_GNB, acc_KNN, acc_per, acc_svm, acc_tree])
+classifiers = {0: 'GNB', 1: 'KNN', 2: 'Perceptron', 3: 'SVM', 4: 'DecisionTree'}
+print('Best gender classifier is {}'.format(classifiers[index]))
